19bf190382fdb0e964fad0d7b6a3ecbb22ce9c23
Roland <roland@strein9.at>
2016-06-09 12:16:00 +0200

    included license for pypi upload

--- nidhogg/setup.py
+++ nidhogg/setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from setuptools import setup, find_packages
+
+
+with open('../README.rst') as file:
+    long_description = file.read()
+
+with open('../LICENSE.txt') as file:
+    license = file.read()
 
 setup(
     name='nidhogg',
     version='3.0',
-    description='NetApp Interface done right',
-    long_description=open('../doc/source/index.rst').read(),
-    author='Chrtian Assing, Roland Wohlfahrt',
+    description='wrapper interface to Netapp filers',
+    long_description=long_description,
+    author='Christian Assing, Roland Wohlfahrt',
     author_email='christian.assing@infineon.com, roland.wohlfahrt-ee@infineon.com',
     url='https://github.com/ifxit/nidhogg',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=open("requirements.txt").readlines(),
-    license="MIT License",
+    license=license,
     platforms='any',
     keywords='netapp vserver sevenmode ontapi',
     classifiers=[
