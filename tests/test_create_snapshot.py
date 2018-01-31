cca6a6292bfc1ae2f00a07635e5700e5886eab3d
Roland Wohlfahrt <i@rolze.com>
2018-01-31 13:24:51 +0100

    create_snapshot simplified, function does not return a dict any more

--- tests/test_create_snapshot.py
+++ tests/test_create_snapshot.py
@@ -1,30 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import pytest
-
-from nidhogg.sevenmode import SevenMode
-from nidhogg.clustermode import ClusterMode
-
-
-@pytest.mark.parametrize('mode', [
-    (ClusterMode, {}),
-    (SevenMode, {})
-], indirect=True)
-def test_create_snapshot(mode):
-    assert mode.create_snapshot("vol", "haha") == {'netapp': {'results': {}}}
-
 
 def test_create_snapshot_api(allmodes):
     allmodes.create_snapshot("vol", "haha")
     assert allmodes.sent == [('snapshot_create', {'volume': 'vol', 'snapshot': 'haha'})]
-
-
-@pytest.mark.parametrize('mode', [
-    (ClusterMode, {}),
-], indirect=True)
-def test_create_snapshot_wit_label(mode):
-    assert mode.create_snapshot("vol", "haha", "supalabel") == {'netapp': {'results': {}}}
 
 
 def test_create_snapshot_wit_label_api(clustermode):
