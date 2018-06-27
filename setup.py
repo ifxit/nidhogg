112ccf61e860caaaf5ae465244555295647aa411
Roland Wohlfahrt <i@rolze.com>
2018-06-27 22:54:01 +0200

    current dev version bumped to 3.7.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name='nidhogg',
-    version='3.7.1',
+    version='3.7.2',
     description='wrapper interface to Netapp filers',
     long_description=read("README.rst"),
     author='Christian Assing, Roland Wohlfahrt',
